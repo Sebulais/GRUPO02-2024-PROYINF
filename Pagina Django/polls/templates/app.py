fdde64b14b7885ad9dba8e59f8411cbebb1829e1
Belei <benjamin.leiton@usm.cl>
2024-11-25 23:55:41 -0300

    Pagina final

--- Pagina Django/polls/templates/app.py
+++ Pagina Django/polls/templates/app.py
@@ -1,12 +1,41 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+import os
+import google.generativeai as genai
 
-@app.route('/procesar_mensaje', methods=['POST'])
-def chatbot():
-    user_input = request.json.get('message')
-    # Aquí procesas `user_input` con tu IA y generas `bot_reply`
-    bot_reply = f"Respuesta generada para: {user_input}"  # Ejemplo básico de respuesta
-    return jsonify({"reply": bot_reply})
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  system_instruction="You'll speak in Spanish from now on. You're a specialist AI that helps in the FIA Chile.",
+)
+
+history = []
+
+print("FIA IA: Hola, soy la IA de la FIA Chile. ¿En qué puedo ayudarte hoy?")
+
+while True:
+
+    user_input = input("You: ")
+
+    chat_session = model.start_chat(
+        history = history
+    )
+
+    response = chat_session.send_message("user_input")
+
+    model_response = response.text
+
+    print(model_response)
+    print()
+
+    history.append("role": "user", "parts": [user_input])
+    history.append("role": "system", "parts": [model_response])
--- Pagina Django/polls/urls.py
+++ Pagina Django/polls/urls.py
@@ -2,7 +2,12 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from . import views
+from django.urls import path
+from .views import chat_ia
 urlpatterns = [
+    path("api/chat/", views.chat_ia, name="chat_ia"),
+    path("contacto/", chat_ia, name="contacto"),
+    path("", chat_ia, name="chat_ia"),  # URL para las peticiones AJAX
     path("", views.index, name="index"),
     path("", views.home, name="home"),
     path("", views.boletines, name="boletines"),
