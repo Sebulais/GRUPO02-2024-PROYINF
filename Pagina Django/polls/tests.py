a92755cf84322ef5c207c9abc3d8847461b09b1a
Sebulais <69325249+Sebulais@users.noreply.github.com>
2025-05-11 22:43:38 -0400

    Add files via upload

--- Pagina Django/polls/tests.py
+++ Pagina Django/polls/tests.py
@@ -1,3 +1,15 @@
+import json
 from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Boletin
 # Create your tests here.
+
+class APITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.boletin = Boletin.objects.create(titulo="Boletín 1", estado="Borrador")
+
+
+# Ejecutar casos pruebas en la terminal con este comando: python manage.py test.
+
--- Pagina Django/polls/utils.py
+++ Pagina Django/polls/utils.py
@@ -1,4 +1,6 @@
 from google.cloud import translate
+from django.core.mail import send_mail
+from .models import Usuario
 
 def traducir_texto(texto, idioma_destino='en'):
     # Crear cliente de Google Translate
@@ -24,3 +26,24 @@ def traducir_texto(texto, idioma_destino='en'):
 
 
     return response.translations[0].translated_text
+
+
+def notificar(evento, objeto):
+    if evento == 'nueva_info_relevante':
+        asunto = "Nueva información científica relevante"
+        mensaje = f"Se ha registrado nueva información relevante: {objeto.titulo}"
+        usuarios = Usuario.objects.filter(rol__in=['coordinadora', 'editor'])
+
+    elif evento == 'boletin_listo':
+        asunto = "Boletín listo para revisión"
+        mensaje = f"El boletín '{objeto.titulo}' está listo para revisión."
+        usuarios = Usuario.objects.filter(rol='coordinadora')
+
+    for usuario in usuarios:
+        send_mail(
+            asunto,
+            mensaje,
+            'noreply@tuapp.com',
+            [usuario.email],
+            fail_silently=False,
+        )
